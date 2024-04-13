dc33733aa9736a6d35c7c8e28acc73ae3eb630f6
Job Ferreira <jobin.ferreira@gmail.com>
2024-04-12 22:12:39 -0300

    conclusão

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone smartphoneNokia = new Nokia("92422332", "T20", "LO2SJI4", 16);
+smartphoneNokia.Ligar();
+smartphoneNokia.InstalarAplicativo("Whatsapp");
+
+
+Smartphone smartphoneIphone = new Iphone("924645632", "8s", "RO2FSI4", 32);
+smartphoneIphone.Ligar();
+smartphoneIphone.InstalarAplicativo("Instagram");
